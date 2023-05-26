d169373aa0c5bb805f01b5d911524fcf284b7e2b
Hossam Shehab <hossam-shehab@hotmail.com>
2023-05-26 17:40:36 +0300

    First Commit

--- commonjs/dynamo-handler/otel.js
+++ commonjs/dynamo-handler/otel.js
@@ -1,27 +1,44 @@
-const { SimpleSpanProcessor, ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-base");
+const { BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
 const { AwsLambdaInstrumentation } = require('@opentelemetry/instrumentation-aws-lambda');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
-
 const { AWSXRayIdGenerator } = require('@opentelemetry/id-generator-aws-xray');
 
-// OTel JS - Core - Exporters
-// const { CollectorTraceExporter } = require('@opentelemetry/exporter-collector-grpc');
+const { SemanticResourceAttributes,} = require('@opentelemetry/semantic-conventions');
+const { Resource } = require('@opentelemetry/resources');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
+///////////////////////////////////////////////////////////////////////////////////
 
-const provider = new NodeTracerProvider({idGenerator: new AWSXRayIdGenerator()});
-provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+const providerConfig = {
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: process.env.AWS_LAMBDA_FUNCTION_NAME,
+  }),
+  idGenerator: new AWSXRayIdGenerator()
+};
+const provider = new NodeTracerProvider(providerConfig);
+
+// Console Exporter
+//provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+
+// OTLP GRPC Exporter
+const collectorOptions = {
+  url: process.env.OTEL_COLLECTOR,
+};
+const exporter = new OTLPTraceExporter(collectorOptions);
+provider.addSpanProcessor(new BatchSpanProcessor(exporter));
 
 // Expects Collector at env variable `OTEL_EXPORTER_OTLP_ENDPOINT`, otherwise, http://localhost:4317
-// provider.addSpanProcessor(new SimpleSpanProcessor(new CollectorTraceExporter()));
 
 provider.register();
 
 registerInstrumentations({
  instrumentations: [
    getNodeAutoInstrumentations(),
-   new AwsLambdaInstrumentation({
-     disableAwsContextPropagation: false
-   })
+   new AwsLambdaInstrumentation(),
+   "@opentelemetry/instrumentation-aws-sdk",
+   "@opentelemetry/instrumentation-http",
+   "@opentelemetry/instrumentation-graphql",
+   "@opentelemetry/instrumentation-mongodb"
  ],
 });
